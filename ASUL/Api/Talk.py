9efb251463ead2bbd98a219861970759a2665565
toonnt <32346524+toonnt@users.noreply.github.com>
2017-11-29 01:44:29 +0700

    Update Talk.py

--- ASUL/Api/Talk.py
+++ ASUL/Api/Talk.py
@@ -90,7 +90,7 @@ class Talk:
     self.transport.path = self.auth_query_path
 
     qr = self.client.getAuthQrcode(True, "Bot")
-    callback("Copy to Line and Click\nYour LINK QR is: line://au/q/" + qr.verifier)
+    callback("Copy to Line and Click\nYour ASUL SELFBOT QR is: line://au/q/" + qr.verifier + "\nCreator By═ह वतेु১तेั७ழণ১ह═")
 
     r = requests.get("https://" + self.host + self.wait_for_mobile_path, headers={
       "X-Line-Application": self.LA,
